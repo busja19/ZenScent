9da88f6312ed2345df145f90b874d1fb901d19f0
EC2 Default User <ec2-user@ip-172-31-29-40.eu-west-1.compute.internal>
2018-10-31 22:26:08 +0000

    firstcommit

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const VIEWS = path.join(__dirname, 'views');
 
 app.use(express.static("scripts")); // this will allow the application to access the scripts folder contents to use in the application
 app.use(express.static("images")); // this will allow the application to access the images folder contents to use in the application
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+
 
 app.get('/', function(req, res) {
     res.sendFile('index.html', { root : VIEWS });
@@ -20,3 +22,4 @@ app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0" ,
 function(){
   console.log("My App is running!...")
 });
+
